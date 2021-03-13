94ee8081ee05e03cb8de53fdfb7831caab783ec2
ghapsara <guruhhapsara@gmail.com>
2021-03-14 01:26:30 +0700

    fix directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import { device } from "../utils/device"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Container from "../components/container"
-import Hero from "../components/hero-1/index"
+import Hero from "../components/hero"
 import ContentWrapper from "../components/content"
 
 const Title = styled.h1`
